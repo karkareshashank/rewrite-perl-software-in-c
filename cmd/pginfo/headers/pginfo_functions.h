6b74f8d5d68f704f1de9a9b2bdc6cfd6883a1e30
shashank <shashank@openindiana>
2011-08-23 20:17:16 +0530

    pginfo updated

---
+++ cmd/pginfo/headers/pginfo_functions.h
@@ -0,0 +1,18 @@
+#ifndef __pginfo_functions_H__
+#define	__pginfo_functions_H__
+
+static int intcompare(const void *p1, const void *p2);
+
+void cpu_list_fill(char* cpu_list , int* cpus);
+
+
+
+
+
+
+
+
+
+
+
+#endif
